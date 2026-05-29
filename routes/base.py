4a10f3bfff5873abe49da3ec7e105fbb1e064e39
fareselmetwaly <fares.m.elmetwaly@google.com>
2026-05-29 22:05:18 +0300

    feat: clean upload for tut-002

---
+++ routes/base.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI,APIRouter
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+
+)
+
+@base_router.get("/")
+def welcome():
+    return {"message": "Welcome to FastAPI!"}   
